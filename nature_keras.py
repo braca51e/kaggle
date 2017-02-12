54f36497dc548672819bb85a5b5e95bca68bd36f
braca51e <braca51e@gmail.com>
2017-02-11 21:22:01 -0600

    Update nature_keras.py

--- nature_keras.py
+++ nature_keras.py
@@ -1,5 +1,3 @@
-##THEANO_FLAGS='floatX=float32,device=cpu,lib.cnmem=1' python <myscript>.py
-
 ############## ALL IMPRTS ########################
 from __future__ import print_function
 import numpy as np
@@ -14,7 +12,7 @@ from keras.optimizers import Adadelta, SGD
 from keras.callbacks import History, ModelCheckpoint, LambdaCallback
 from keras.preprocessing.image import ImageDataGenerator
 from keras.applications.inception_v3 import InceptionV3
-from keras import backend as K
+from keras.applications.vgg16 import VGG16
 
 import pandas as pd
 
@@ -35,37 +33,36 @@ def plot_progress(epoch,logs):
 
 #K.set_image_dim_ordering('th') # For image align
 
-batch_size = 32
+batch_size = 100
 nb_epoch = 50
 nb_classes = 8
-im_shape = (320, 180)
-im_shape_dim = (320, 180, 3)
+im_shape = (224, 224)
+im_shape_dim = (224, 224, 3)
 
-locpath = "/Users/piris/Documents/Kaggle/Nature/trainingResults/"
+locpath = "/home/braca/Documents/Kaggle/Nature/trainingResults/classify/"
 filepath = locpath+"net.{epoch:02d}-{val_loss:.2f}.h5"
-weights_path = "/Users/piris/Documents/Kaggle/Nature/weights_pretrained/vgg16_weights.h5"
 
 ############## LOAD TRAINING DATA ########################
 
 train_datagen = ImageDataGenerator(
                                    rescale=1./255,
-                                   rotation_range=40,
-                                   width_shift_range=0.3,
-                                   height_shift_range=0.3,
-                                   shear_range=0.3,
-                                   zoom_range=0.3,
+                                   rotation_range=30,
+                                   width_shift_range=0.2,
+                                   height_shift_range=0.2,
+                                   shear_range=0.2,
+                                   zoom_range=0.2,
                                    horizontal_flip=True)
 
 test_datagen = ImageDataGenerator(rescale=1./255)
 
 train_generator = train_datagen.flow_from_directory(
-                '../data/train',  # this is the target directory
+                '/home/braca/Documents/Kaggle/Nature/data/train_bin/train/',
                 target_size=(im_shape),  # all images will be resized to 224x224
                 batch_size=batch_size,
                 class_mode='categorical')  # since there are 8 different fish labels
 
 validation_generator = test_datagen.flow_from_directory(
-                '../data/val',
+                '/home/braca/Documents/Kaggle/Nature/data/train_bin/val/',
                 target_size=(im_shape),
                 batch_size=batch_size,
                 class_mode='categorical')
@@ -79,26 +76,24 @@ base_net = InceptionV3(weights='imagenet', include_top=False,input_shape=im_shap
 x = base_net.output
 x = GlobalAveragePooling2D()(x)
 # let's add a fully-connected layer
-x = Dense(1024, activation='relu')(x)
-# and a logistic layer -- we have 8 classes
-predictions = Dense(8, activation='softmax')(x)
+x = Dense(4096, activation='relu')(x)
+# and a logistic layer -- we have 7 classes no nof
+predictions = Dense(2, activation='softmax')(x)
 
 # this is the model we will train
 net = Model(input=base_net.input, output=predictions)
 
 # first: train only the top layers (which were randomly initialized)
 # i.e. freeze all convolutional InceptionV3 layers
-for layer in net.layers[:210]:
+for layer in base_net.layers:
     layer.trainable = False
-for layer in net.layers[210:]:
-    layer.trainable = True
 
 #opt = RMSprop(lr=0.01, rho=0.9, epsilon=1e-08, decay=0.0)
 #opt = Adadelta()
-opt=SGD(lr=0.001, momentum=0.9)
+opt=SGD(lr=0.01, momentum=0.9)
 
-net.compile(loss='categorical_crossentropy',
-            optimizer=opt,
+net.compile(loss='binary_crossentropy',
+            optimizer='adam',
             metrics=['accuracy'])
 
 history = History()
@@ -107,16 +102,36 @@ plot_progress_cb = LambdaCallback(on_epoch_end=plot_progress)
 check = ModelCheckpoint(filepath=filepath,monitor='val_acc', verbose=1,
                         save_best_only=True)
 
-print("Starting training...")
+print("Starting training 1...")
 
 net.fit_generator(
             train_generator,
-            samples_per_epoch=3390,
-            nb_epoch=nb_epoch,
+            samples_per_epoch=3440,
+            nb_epoch=12,
             verbose=1,
             callbacks=[history,check,plot_progress_cb],
             validation_data=validation_generator,
-            nb_val_samples=357)
+            nb_val_samples=337)
 
+# we chose to train the top 2 inception blocks, i.e. we will freeze
+# the first 172 layers and unfreeze the rest:
+for layer in net.layers[:172]:
+    layer.trainable = False
+for layer in net.layers[172:]:
+    layer.trainable = True
+
+net.compile(loss='binary_crossentropy',
+            optimizer=opt,
+            metrics=['accuracy'])
+
+print("Starting training 2...")
+net.fit_generator(
+                  train_generator,
+                  samples_per_epoch=3440,
+                  nb_epoch=nb_epoch,
+                  verbose=1,
+                  callbacks=[history,check,plot_progress_cb],
+                  validation_data=validation_generator,
+                  nb_val_samples=337)
 
 print("Training end...")
